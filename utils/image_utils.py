8ca5a6bf3c7641057b0264204badfcaad593676e
Ashmeen Kaur <149650309+ashmeenkhaira@users.noreply.github.com>
2026-01-18 23:08:20 +0530

    Update image_utils.py

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -1,37 +1,16 @@
 import tensorflow as tf
+import numpy as np
+from PIL import Image
 
-DISEASES = [
-    "Pneumonia",
-    "Edema",
-    "Effusion",
-    "Cardiomegaly",
-    "Atelectasis"
-]
+IMG_SIZE = 224
 
-def load_vocab(path="model/vocab.txt"):
-    with open(path) as f:
-        return [line.strip() for line in f]
-
-def generate_report(model, img_f, img_l, vocab, max_len=149):
-    decoder_input = tf.zeros((1, max_len), dtype=tf.int64)
-
-    for i in range(max_len - 1):
-        label_pred, text_pred = model.predict(
-            [img_f, img_l, decoder_input],
-            verbose=0
-        )
-
-        next_token = tf.argmax(text_pred[0, i]).numpy()
-        decoder_input = tf.tensor_scatter_nd_update(
-            decoder_input, [[0, i + 1]], [next_token]
-        )
-
-        if next_token == 0:
-            break
-
-    words = [vocab[t] for t in decoder_input.numpy()[0] if t > 0]
-    report = " ".join(words)
-
-    disease_probs = dict(zip(DISEASES, label_pred[0]))
-
-    return report, disease_probs
+def preprocess_image(image_file):
+    """
+    image_file: Uploaded file from Streamlit
+    returns: (1, 224, 224, 3) float32 tensor
+    """
+    image = Image.open(image_file).convert("RGB")
+    image = image.resize((IMG_SIZE, IMG_SIZE))
+    image = np.array(image) / 255.0
+    image = image.astype("float32")
+    return tf.expand_dims(image, axis=0)
